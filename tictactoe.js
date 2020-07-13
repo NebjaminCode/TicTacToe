ee7bc4a3711fcf5548824052e6cb4813a8573e4c
Benjamin Code <bjamescode@gmail.com>
2020-07-13 15:26:31 -0700

    added player factory function and began sorting out logic

---
+++ tictactoe.js
@@ -0,0 +1,41 @@
+// create a "create players" factory function that does either of the following:
+//     - user vs AI
+//     - user vs user
+
+// create a module that: 
+//     - shows players
+//     - keeps score
+
+/*
+what does a player need to do:
+- have an X or an O sign
+- have a total game score
+- have a name
+*/
+
+const players = (name) => {
+    let score = 0;
+    let playerName = () => name;
+    let scoreIncrease = () => score++;
+    let newScore = () => score;
+
+    return {playerName, scoreIncrease, newScore};
+}
+
+
+var setup = (function() {
+    var square = document.getElementsByClassName('square');
+    
+    function gameplay() {
+
+    }
+
+    function gameboard() {
+        [
+            ['','',''],
+            ['','',''],
+            ['','','']
+        ]
+    }          
+
+})();

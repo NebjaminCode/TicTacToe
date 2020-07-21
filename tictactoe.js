6032cd072e943f17011f98f973d722c2df0fe787
Benjamin Code <bjamescode@gmail.com>
2020-07-20 20:32:12 -0700

    minor style update and wrote out some thoughts on how to proceed with js

--- tictactoe.js
+++ tictactoe.js
@@ -1,17 +1,22 @@
-// create a "create players" factory function that does either of the following:
-//     - user vs AI
-//     - user vs user
-
-// create a module that: 
-//     - shows players
-//     - keeps score
+/* 
+create a "create players" factory function that does either of the following:
+     - user vs AI
+     - user vs user
+*/
 
 /*
 what does a player need to do:
-- have an X or an O sign
-- have a total game score
-- have a name
+    - have an X or an O sign
+    - have a total game score
+    - have a name
 */
+
+/* 
+create a module that: 
+     - shows players
+     - keeps score
+*/
+
 
 // Player factory function
 const players = (name) => {
@@ -23,20 +28,12 @@ const players = (name) => {
     return {playerName, scoreIncrease, newScore};
 }
 
+// Gameboard setup
+/*
+I think the general idea here is that once a space on the gameboard is clicked (which will be controlled by the gamplay module) the array will be altered. gameboard[0] would change to X or O when the top left square in the game is clicked, for example. Then, we can judge which player wins by creating an array of winning combinations and compare the two. Ex: gameboard['X', 'X', 'X', 'O', 'O', '', '',...] would declare player X the winner, since that's 3 squares in a row. 
+*/
+const setup = (() => {
+    let gameboard = ['','','','','','','','',''];
 
-var setup = (function() {
-    var square = document.getElementsByClassName('square');
-    
-    function gameplay() {
-
-    }
-
-    function gameboard() {
-        [
-            ['','',''],
-            ['','',''],
-            ['','','']
-        ]
-    }          
-
+    return {gameboard};
 })();

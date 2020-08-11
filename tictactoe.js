93ce7c73d7190723f4963c780f3a32d2f8977398
Benjamin Code <bjamescode@gmail.com>
2020-08-10 21:32:33 -0700

    fixed scoreboard increase function and added shadows

--- tictactoe.js
+++ tictactoe.js
@@ -32,6 +32,7 @@ const players = (nameInput) => {
 let user = players("user");
 let ai = players('AI');
 
+
 // Gameboard setup
 /*
 I think the general idea here is that once a space on the gameboard is clicked (which will be controlled by the gamplay module) the array will be altered. gameboard[0] would change to X or O when the top left square in the game is clicked, for example. Then, we can judge which player wins by creating an array of winning combinations and compare the two. Ex: gameboard['X', 'X', 'X', 'O', 'O', '', '',...] would declare player X the winner, since that's 3 squares in a row. 
@@ -66,8 +67,10 @@ const gameplay = (() => {
                 setup.gameboard[0] == 'x' && setup.gameboard[4] == 'x' && setup.gameboard[8] == 'x' || 
                 setup.gameboard[6] == 'x' && setup.gameboard[4] == 'x' && setup.gameboard[2] == 'x'
                ) {
-                    user.scoreIncrease;
+                    user.score = user.score + 1;
                     alert("you win!");
+                    console.log(user.score)
+                    scoreDisplay()
                     setTimeout (function(){autoReset()}, 1000);
                     return;
                 } else {
@@ -96,15 +99,17 @@ const gameplay = (() => {
     }
 })();
 
-const scoreDisplay = (() => {
+const scoreDisplay = () => {
     const playerScore = document.querySelector("#playerHolder");
     const compScore = document.getElementById("compHolder")
-    playerScore.innerHTML = user.newScore;
-    compScore.innerHTML = ai.newScore;
-})();
+    playerScore.innerHTML = user.score;
+    compScore.innerHTML = ai.score;
+};
+
+scoreDisplay()
 
 function AI () {
-    if (moveCount >= 10) {
+    if (moveCount >= 18) {
         return
     }
     let CompMove = Math.floor((Math.random() * 7) + 1);
@@ -113,9 +118,6 @@ function AI () {
                 setup.gameboard[CompMove] = "o";
                 console.log(`CompMove = ${CompMove}`)
                 moveCount++;
-                return;
-        } else {
-            AI();
                 if (
                     setup.gameboard[0] == 'o' && setup.gameboard[1] == 'o' && setup.gameboard[2] == 'o' ||
                     setup.gameboard[3] == 'o' && setup.gameboard[4] == 'o' && setup.gameboard[5] == 'o' ||
@@ -126,11 +128,16 @@ function AI () {
                     setup.gameboard[0] == 'o' && setup.gameboard[4] == 'o' && setup.gameboard[8] == 'o' || 
                     setup.gameboard[6] == 'o' && setup.gameboard[4] == 'o' && setup.gameboard[2] == 'o'
                 ) {
-                    ai.scoreIncrease;
+                    ai.score = ai.score + 1;
                     alert("You Lose!");
+                    scoreDisplay();
                     autoReset();
                     return;
-                }      
+                }   
+            return;
+        } else {
+            AI();
+                   
         }
 }
 
